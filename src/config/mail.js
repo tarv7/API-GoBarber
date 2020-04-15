911b00df94dfc51ad22e0461196711c3fb824357
Thales Augusto <thallles.ramos@gmail.com>
2020-04-14 21:54:14 -0300

    Configurando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7f70be899dd1bb',
+    pass: '50a7ed9fd84281',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
